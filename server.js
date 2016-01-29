4990c2f10b05b50e2d960d83bb3be134be80fb10
mzadig <m.zadig@comcast.net>
2016-01-29 13:20:17 +0000

    modified:   index.jade 	modified:   package.json 	modified:   server.js
     Untracked files:
    	models/
    	mongod

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var jobModel = require('./models/Job');
 
 var app = express();
 
@@ -7,8 +9,31 @@ app.set('views', __dirname);
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
+// Add route to the database
+// But before the catchall
+app.get('/api/jobs', function(req,res){
+    mongoose.model('Job').find({}).exec(function(error,collection){
+        res.send(collection);
+    })
+});
+
+//
+//Catchall
 app.get('*', function(req,res){
     res.render('index');
 })
+
+
+
+//mongoose.connect('mongodb://localhost/jobfinder');
+mongoose.connect('mongodb://sa2:*administrator*@ds051575.mongolab.com:51575/jobfinder-mz');
+
+var conn = mongoose.connection;
+
+conn.once('open',function() {
+    console.log('connected to mongodb successfully!');
+    jobModel.seedJobs();
+    
+});
 
 app.listen(process.env.PORT, process.env.IP);
